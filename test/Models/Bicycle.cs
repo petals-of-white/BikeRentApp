a7164dbf0ea77219910f8adaa5b4c6c809b3c723
Maksym Syvash <maxim.sivash@gmail.com>
2022-01-08 10:18:38 +0200

    added models for bicycle and bycicle type

---
+++ test/Models/Bicycle.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace test.Models
+{
+    public class Bicycle
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        //[Column(TypeName = "varchar(100)")]
+        public string Name { get; set; }
+
+        [Required]
+        public BicycleType BicycleType { get; set; }
+
+        [Required]
+        public decimal RentPrice { get; set; }
+
+        [Required]
+
+        public bool IsRented { get; set; }
+
+
+    }
+}
