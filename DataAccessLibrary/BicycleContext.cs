a226f25c0487aaf3eda44a234f8ffc2dd6efae0d
Maksym Syvash <maxim.sivash@gmail.com>
2022-01-08 10:49:41 +0200

    added data access project. added a dbContext class

---
+++ DataAccessLibrary/BicycleContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using EFAccess.Models;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace DataAccessLibrary
+{
+    public class BicycleContext : DbContext
+    {
+        public DbSet<Bicycle> Bicycles { get; set; }
+        public DbSet<BicycleType> BicycleTypes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            
+            IConfigurationRoot config = builder.Build();
+
+            options.UseSqlServer(config.GetConnectionString("Default"));
+        }
+    }
+}
